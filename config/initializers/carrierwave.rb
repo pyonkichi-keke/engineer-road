4c55c0c2f068102c127e94fdd8c2a7033b78d1b8
pyonkichi-keke <pyonkichi.keke.sakura@gmail.com>
2020-07-20 16:10:45 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,27 +5,12 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
   config.storage = :fog
   config.fog_provider = 'fog/aws'
-  config.asset_host = 'https://s3.amazonaws.com/engineer-road'
-    # NOTE: AWS側の設定を変えなくても、この１行の設定でアップロードできた
-  config.fog_public = false # ←コレ
-  # config.fog_credentials = {
-  #   provider: 'AWS',
-  #   aws_access_key_id: "AWS_ACCESS_KEY_ID",
-  #   aws_secret_access_key: "AWS_SECRET_ACCESS_KEY",
-  #   region: 'ap-northeast-1'
-  # }
-
-  if Rails.env.production?
-    CarrierWave.configure do |config|
-      config.fog_credentials = {
-        # Configuration for Amazon S3
-        :provider              => 'AWS',
-        :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-        :aws_secret_access_key => ENV['S3_SECRET_KEY']
-      }
-      config.fog_directory     =  ENV['S3_BUCKET']
-    end
-  end
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+    region: 'ap-northeast-1'
+  }
 
   config.fog_directory  = 'engineer-road'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/engineer-road'
