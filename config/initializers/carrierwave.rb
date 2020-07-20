6f47a89787bc61fc2d242616f63484a348ce3e36
pyonkichi-keke <pyonkichi.keke.sakura@gmail.com>
2020-07-20 15:34:00 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,10 +5,13 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
   config.storage = :fog
   config.fog_provider = 'fog/aws'
+  config.asset_host = 'https://s3.amazonaws.com/engineer-road'
+    # NOTE: AWS側の設定を変えなくても、この１行の設定でアップロードできた
+  config.fog_public = false # ←コレ
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+    aws_access_key_id: "AWS_ACCESS_KEY_ID",
+    aws_secret_access_key: "AWS_SECRET_ACCESS_KEY",
     region: 'ap-northeast-1'
   }
 
