241e91c7877536e561a9c21879f2691904b01970
pyonkichi-keke <pyonkichi.keke.sakura@gmail.com>
2020-08-25 14:09:27 +0900

    修正

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -15,7 +15,7 @@ $(document).on('turbolinks:load', ()=> {
                 `
     return html;
   }
-  $('#new_comment').on('submit', function(e){
+  $('.message_main_list_comment_form').on('submit', '#new_comment', function(e){
     e.preventDefault();
     let formData = new FormData(this);
     let url = $(this).attr('action')
@@ -29,13 +29,15 @@ $(document).on('turbolinks:load', ()=> {
   })
     .done(function(data){
       let html = buildHTML(data); 
-      $(`.message_main_list_comment_area[data-index="${data.message_id}"`).append(html);
+      let num = data["message_id"]
+      $(`.message_main_list_comment_area[data-index= "${num}"]`).append(html);
       $('.message_main_list_comment_form_text').val('');
       $('#comment_send').prop('disabled', false);
-      $(`div[data-index="${data.message_id}"`).animate({scrollTop: $(`div[data-index="${data.message_id}"`)[0].scrollHeight });
+      $(`.message_main_list_comment_area[data-index= "${num}"]`).animate({scrollTop: $(`.message_main_list_comment_area[data-index= "${num}"]`)[0].scrollHeight });
     })
     .fail(function(){
       alert('コメントを行うにはログインが必要です');
     })
   })
+
 });
--- app/views/comments/create.json.jbuilder
+++ app/views/comments/create.json.jbuilder
@@ -2,6 +2,3 @@ json.comment  @comment.comment
 json.user_id  current_user.id
 json.message_id  @comment.message.id
 json.user  @comment.user
-
-
-
